dfc8715b48b4356ab56501b7557a96b4473e1f35
Oleksii Esaulov <win.city18@gmail.com>
2021-06-16 12:40:51 +0300

    16 june newmysql db

--- src/__init__.py
+++ src/__init__.py
@@ -16,8 +16,10 @@ app.secret_key = "Secret Key"
 if os.environ.get('IMONHEROKU') == 'TRUE':
     # app.config[
     #    'SQLALCHEMY_DATABASE_URI'] = 'mysql://b28bfef9567dc7:4956928b@eu-cdbr-west-01.cleardb.com/heroku_380d0cff17aa9f2'
+    # app.config[
+    #    'SQLALCHEMY_DATABASE_URI'] = "mysql://sql11416587:hiVSf2GwPt@sql11.freemysqlhosting.net/sql11416587"
     app.config[
-        'SQLALCHEMY_DATABASE_URI'] = "mysql://sql11416587:hiVSf2GwPt@sql11.freemysqlhosting.net/sql11416587"
+        'SQLALCHEMY_DATABASE_URI'] = "mysql://WEervXQ4cy:3ExKIOdJrT@remotemysql.com/WEervXQ4cy"
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/finalproject"
 
